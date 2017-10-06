42a468fc90f870503a5f109011f22ccae0ba97d9
Luke West <luke@59club.com>
2017-10-06 20:03:30 +0100

    Added jquery + work on layout/design

--- application/views/home.php
+++ application/views/home.php
@@ -16,7 +16,6 @@
 <html lang="en">
 
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -26,13 +25,12 @@
     <link href="<?php echo(base_url("/assets/css/main.css")) ?>" rel="stylesheet" type="text/css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-
+    <link rel="icon" href="<?=base_url()?>/assets/images/unlock_world.png" type="image/gif">
   </head>
 
   <body>
@@ -46,13 +44,19 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <a class="nav-link aboutTab" href="#">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Blog</a>
+              <a class="nav-link o2oTab" href="#">One to One Sessions</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+              <a class="nav-link myStoryTab" href="#">My Story</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link wellbeingTab" href="#">Wellbeing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link proSpeakerTab" href="#">Professional Speaker</a>
             </li>
           </ul>
         </div>
@@ -69,7 +73,7 @@
       </div>
     </header>
 
-    <section class="content-section-a"  >
+    <section class="content-section-a" id="about">
       <div class="container">
         <div class="row">
           <div class="col-lg-6 ml-auto">
@@ -91,7 +95,7 @@
       </div>
     </section>
 
-    <section class="content-section-b">
+    <section class="content-section-b" id="one_to_one">
 
       <div class="container">
 
@@ -154,45 +158,42 @@
               <li>losing weight</li>
               <li>personal development</li>
               <li>phobias</li>
-              <li>procrastination</li>
-              <li>public speaking</li>
             </ul>
           </div>
           <div class="col-lg-5 mr-auto" style="display: inline-block;">
             <ul>
+              <li>procrastination</li>
+              <li>public speaking</li>
               <li>stress</li>
-              <li>unwanted feelings anger</li>
-              <li>frustration</li>
-              <li>shame</li>
-              <li>guilt</li>
-              <li>abandonment</li>
-              <li>regret</li>     
+              <li>unwanted feelings anger, frustration, shame, guilt, abandonment and regret</li>
               <li>relationship anxieties</li>
               <li>removing your negative feelings attached to your negative memories</li>
             </ul>
           </div>
           <br><br>
-          <p>Usually only one session is needed. After the session people told me they felt relaxed, calmer, lighter, confident and positive.</p>
+          <p>After the session people told me they felt relaxed, calmer, lighter, confident and positive.</p>
         </div>
       </div>
     </section>
 
-    <section class="content-section-a">
+    <section class="content-section-a" id="my_story">
       <div class="container">
         <div class="row">
           <div class="text-center">
             <div class="clearfix"></div>
-            <h2 class="section-heading">My Story (My testimonial)</h2>
-            <p>&quot;I was overweight, caused by emotional over eating and not happy. I’ve used
-            the techniques myself. Working through my emotional baggage using NLP
-            and Havening to clear out my negatives feelings attached to upsetting
-            memories. I no longer have the need to overeat.&quot;</p>
+            <h2 class="section-heading">My Story</h2>
+            <p>&quot;I was overweight, caused by emotional over eating and not happy. I’ve used the techniques myself. Working through my emotional baggage using NLP and Havening to clear out my negatives feelings attached to upsetting memories. I no longer have the need to overeat. Visualising what I would look like, how that would make me feel. Visualising eating small amounts yet feeling full and satisfied and only eating when I was hungry. Changing the images in your head is so powerful and produces positive results.&quot;</p>
           </div>
           </div>
       </div>
     </section>
+<!--             <p>&quot;I was overweight, caused by emotional over eating and not happy. I’ve used
+            the techniques myself. Working through my emotional baggage using NLP
+            and Havening to clear out my negatives feelings attached to upsetting
+            memories. I no longer have the need to overeat.&quot;</p>
+ -->
 
-    <section class="content-section-b"  >
+    <section class="content-section-b" id="wellbeing">
       <div class="container">
         <div class="row">
           <div class="col-lg-12 ml-auto">
@@ -270,7 +271,7 @@
       </div>
     </section>
 
-    <section class="content-section-a">
+    <section class="content-section-a" id="professional_speaker">
       <div class="container">
         <div class="row">
           <div class="text-center">
@@ -303,20 +304,68 @@
       <div class="container">
         <ul class="list-inline">
           <li class="list-inline-item">
-            <a href="#">Home</a>
+            <a class="nav-link aboutTab" href="#">About</a>
           </li>
           <li class="footer-menu-divider list-inline-item">&sdot;</li>
           <li class="list-inline-item">
-            <a href="#about">About</a>
+            <a class="nav-link o2oTab" href="#">One to One Sessions</a>
           </li>
           <li class="footer-menu-divider list-inline-item">&sdot;</li>
           <li class="list-inline-item">
-            <a href="#contact">Contact</a>
+            <a class="nav-link myStoryTab" href="#">My Story</a>
+          </li>
+          <li class="footer-menu-divider list-inline-item">&sdot;</li>
+          <li class="list-inline-item">
+            <a class="nav-link wellbeingTab" href="#">Wellbeing</a>
+          </li>
+          <li class="footer-menu-divider list-inline-item">&sdot;</li>
+          <li class="list-inline-item">
+            <a class="nav-link proSpeakerTab" href="#">Professional Speaker</a>
           </li>
         </ul>
         <p class="copyright text-muted small">Copyright &copy; Narelle Summers 2017. All Rights Reserved</p>
       </div>
     </footer>
   </body>
-
 </html>
+<script>
+  $(function(){
+    $(".aboutTab").click(function(e){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $("#about").offset().top
+      }, 2000);
+    });
+
+    $(".o2oTab").click(function(e){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $("#one_to_one").offset().top
+      }, 2000);
+    });
+    
+    $(".myStoryTab").click(function(e){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $("#my_story").offset().top
+      }, 2000);
+    });
+
+    $(".wellbeingTab").click(function(e){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $("#wellbeing").offset().top
+      }, 2000);
+    });
+
+
+    $(".proSpeakerTab").click(function(e){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $("#professional_speaker").offset().top
+      }, 2000);
+    });
+
+  });
+
+</script>
